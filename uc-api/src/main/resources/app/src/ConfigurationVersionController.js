d66d1df493983a228484f5b067c2097ea160f2a3
melanxoluk <melancholyprogrammer@gmail.com>
2024-06-08 00:24:00 +0500

    wip design uc

--- uc-api/src/main/resources/app/src/ConfigurationVersionController.js
+++ uc-api/src/main/resources/app/src/ConfigurationVersionController.js
@@ -1,11 +1,17 @@
 class ConfigurationVersionController {
   constructor(app) {
     this.app = app;
+    this.dirs = ["first", "second"];
   }
 
   mount(parent) {
-    m.mount(parent, this)
-
+    m.render(parent, this.view())
+    $('.ui.accordion').accordion({exclusive: false});
+    setTimeout(() => {
+      this.dirs = ["first", "second", "third"];
+      console.info("updated");
+      m.render(parent, this.view());
+    }, 3000)
     // todo
     /*this.app.api
       .fetchConfigurationVersion("first", "1.0.0")
@@ -25,13 +31,27 @@ class ConfigurationVersionController {
   }
 
   view() {
-    return m("div", {class:"ui container"}, [
-      m("h1", {class: "ui header"}, [
-        "Configuration",
-        m("div", {class: "sub header"}, "v1.0.0")
-      ]),
-      
-    ])
+    return m("div", {class: "ui masthead vertical segment"},
+      m("div", {class: "ui container"}, [
+
+        m("h1", {class: "ui header"}, [
+          "Configuration",
+          m("div", {class: "sub header"}, "v1.0.0")
+        ]),
+
+        m("dev", {class: "ui tree accordion"},
+          this.dirs.flatMap(dir => {
+            return [
+              m("div", {class: "title"}, [
+                m("i", {class: "folder icon"}),
+                m("div", {class:"header"}, dir)
+              ]),
+              m("div", {class: "content"},
+                m("p", {class: "transition hidden"}, dir)
+              )
+            ]
+          }))
+      ]))
   }
 }
 
